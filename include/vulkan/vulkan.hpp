e0e2f6a6b7f38c367c3bfcbaac49e2c133d2c8c1
Mark Lobodzinski <mark@lunarg.com>
2018-06-26 12:03:58 -0600

    header: Update to version 1.1.78 of the Vulkan hdr
    - update vk.xml
    - update vulkan_core.h
    - updated validusage.json
    - updated vulkan.hpp

--- include/vulkan/vulkan.hpp
+++ include/vulkan/vulkan.hpp
@@ -53,7 +53,7 @@
 # include <cassert>
 # define VULKAN_HPP_ASSERT   assert
 #endif
-static_assert( VK_HEADER_VERSION ==  77 , "Wrong VK_HEADER_VERSION!" );
+static_assert( VK_HEADER_VERSION ==  78 , "Wrong VK_HEADER_VERSION!" );
 
 // 32-bit vulkan is not typesafe for handles, so don't allow copy constructors on this platform by default.
 // To enable this feature on 32-bit platforms please define VULKAN_HPP_TYPESAFE_CONVERSION
