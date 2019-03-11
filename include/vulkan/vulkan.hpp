d05c8df88da98ec1ab3bc600d7f5783b4060895b
Jon Leech <oddhack@sonic.net>
2019-03-11 02:33:59 -0700

    Update for Vulkan-Docs 1.1.103

--- include/vulkan/vulkan.hpp
+++ include/vulkan/vulkan.hpp
@@ -56,7 +56,7 @@
 # define VULKAN_HPP_ASSERT   assert
 #endif
 
-static_assert( VK_HEADER_VERSION ==  102 , "Wrong VK_HEADER_VERSION!" );
+static_assert( VK_HEADER_VERSION ==  103 , "Wrong VK_HEADER_VERSION!" );
 
 // 32-bit vulkan is not typesafe for handles, so don't allow copy constructors on this platform by default.
 // To enable this feature on 32-bit platforms please define VULKAN_HPP_TYPESAFE_CONVERSION
