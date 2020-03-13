111167ca2890c65663031e164c81317c1ae80729
awr1 <41453959+awr1@users.noreply.github.com>
2020-03-13 15:20:13 -0500

    headers: Simplify Non-Dispatchable Handles

--- include/vulkan/vulkan_core.h
+++ include/vulkan/vulkan_core.h
@@ -54,15 +54,7 @@ extern "C" {
 
 
 #define VK_DEFINE_HANDLE(object) typedef struct object##_T* object;
-
-
-#if !defined(VK_DEFINE_NON_DISPATCHABLE_HANDLE)
-#if defined(__LP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__) ) || defined(_M_X64) || defined(__ia64) || defined (_M_IA64) || defined(__aarch64__) || defined(__powerpc64__)
-        #define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef struct object##_T *object;
-#else
-        #define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef uint64_t object;
-#endif
-#endif
+#define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef struct object { uint64_t opaqueHandle; } object;
 
 typedef uint32_t VkFlags;
 typedef uint32_t VkBool32;
