b9c123bdc1f302b8801db9666867689d07b77297
Juan Ramos <juan@lunarg.com>
2023-07-12 12:52:35 -0600

    Add REUSE compliance checker to CI

--- tests/vk_layer.c
+++ tests/vk_layer.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2022-2023 The Khronos Group Inc.
+ * Copyright 2022-2023 Valve Corporation
+ * Copyright 2022-2023 LunarG, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "vulkan/vk_layer.h"
 
 int foobar()
