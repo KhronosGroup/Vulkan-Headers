a523083f727e2b0585047557659a32c6cd0fa195
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-03-21 10:58:54 +0100

    simplified cpp module to not require function loading

--- tests/vk_hpp_module.cpp
+++ tests/vk_hpp_module.cpp
@@ -6,12 +6,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import vulkan_hpp;
-#include <string>
 
 int test_instance()
 {
-    vk::InstanceCreateInfo instanceCreateInfo({}, nullptr);
-    vk::Instance instance = vk::createInstance(instanceCreateInfo);
-    instance.destroy();
-    return 0;
+    return static_cast<int>(vk::makeApiVersion(1, 0, 0, 0));
 }
