ee35cdf237391684953b70626621b290d1c3e6b8
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-03-21 09:21:18 +0000

    Added licenses and used included headers in minimal examples
    Co-authored-by: jpr42 <109434725+jpr42@users.noreply.github.com>

--- tests/vk_hpp.cpp
+++ tests/vk_hpp.cpp
@@ -1,6 +1,13 @@
+/*
+ * Copyright 2025 The Khronos Group Inc.
+ * Copyright 2025 Valve Corporation
+ * Copyright 2025 LunarG, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <vulkan/vulkan.hpp>
 
-int square(int i)
+int header_version()
 {
-    return i * i;
+    return VK_HEADER_VERSION;
 }
